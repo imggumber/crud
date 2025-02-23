f2626ece0b42d1e5f3cf7e39a441257180da6b89
Gurpreet Gumber <imgurpreetgumber@gmail.com>
2025-02-23 16:44:55 +0530

    Develop get all products API

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,9 +3,32 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index() {
+        $products = Product::get();
+        if ($products->count() > 0) {
+            $product = ProductResource::collection($products);
+            return response()->json([
+                'message' => 'All products fetched successfully',
+                'data' => $product,
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'No product available',
+            ], 200);
+        }
+    }
+
+    public function store() {}
+
+    public function show() {}
+
+    public function update() {}
+
+    public function destroy() {}
 }

8e030b7e27e184931f8501782d9a61046dcd4a85
Gurpreet Gumber <imgurpreetgumber@gmail.com>
2025-02-23 18:22:23 +0530

    Display ID

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -16,6 +16,7 @@ class ProductResource extends JsonResource
     {
         // return parent::toArray($request);
         return [
+            'id'          => $this->id,
             'name'        => $this->name,
             'description' => $this->description,
             'price'       => $this->price,
